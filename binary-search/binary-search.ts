36e445de04eb80fe0ce8c1225d7bbee897a7ab01
Jamer Rebolledo <jamerrq@gmail.com>
2023-07-18 12:37:46 -0500

    added solution for binary-search exercise | via Alk

---
+++ binary-search/binary-search.ts
@@ -0,0 +1,15 @@
+export function find(haystack: number[], needle: number): number | never {
+    let left = 0;
+    let right = haystack.length - 1;
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2);
+        if (haystack[middle] === needle) {
+            return middle;
+        } else if (haystack[middle] < needle) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        };
+    };
+    throw new Error('Value not in array');
+};
