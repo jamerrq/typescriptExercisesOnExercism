d3689125cb17878d0bb5f2798d0f7bfe899e0037
Jamer Rebolledo <jamerrq@gmail.com>
2023-09-15 12:43:29 -0500

    [SOLUTION] problem binary-search-tree (medium)

---
+++ binary-search-tree/binary-search-tree.ts
@@ -0,0 +1,50 @@
+export class BinarySearchTree {
+
+  private _data: number
+  private _left: BinarySearchTree | undefined
+  private _right: BinarySearchTree | undefined
+
+  constructor(data: number) {
+    this._data = data
+  }
+
+  public get data(): number {
+    return this._data
+  }
+
+  public get right(): BinarySearchTree | undefined {
+    return this._right
+  }
+
+  public get left(): BinarySearchTree | undefined {
+    return this._left
+  }
+
+  public insert(item: number): void {
+    // in case of duplicates, insert to the left
+    if (item <= this._data) {
+      if (this._left) {
+        this._left.insert(item)
+      } else {
+        this._left = new BinarySearchTree(item)
+      }
+    }
+    else {
+      if (this._right) {
+        this._right.insert(item)
+      } else {
+        this._right = new BinarySearchTree(item)
+      }
+    }
+  }
+
+  public each(callback: (data: number) => void): void {
+    if (this._left) {
+      this._left.each(callback)
+    }
+    callback(this._data)
+    if (this._right) {
+      this._right.each(callback)
+    }
+  }
+}
