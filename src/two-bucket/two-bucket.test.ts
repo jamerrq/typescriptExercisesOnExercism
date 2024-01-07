37e09189d138cf6fb431b6f6fc6d1ea1047a5054
jamerrq <jamerrq@gmail.com>
2024-01-07 15:31:08 -0500

    🪣 add Two Bucket Solution

--- src/two-bucket/two-bucket.test.ts
+++ src/two-bucket/two-bucket.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest'
 import { TwoBucket } from './two-bucket'
 
 describe('TwoBucket', () => {
--- src/two-bucket/two-bucket.ts
+++ src/two-bucket/two-bucket.ts
@@ -6,103 +6,59 @@ function gcd (a: number, b: number): number {
 }
 
 export class TwoBucket {
+  private _goalBucket = 'one'
+  private _otherBucket = 0
   private _goal: number
+  private _steps = 0
+  private _starterBuck: boolean
   private _buckOne: number
   private _buckTwo: number
-  private _starterBuck: boolean
-  private _goalBucket: string
-  private _otherBucket: number
-  private _status_cache: { [key: string]: number }
 
   constructor(buckOne: number, buckTwo: number,
     goal: number, starterBuck: string) {
     this._goal = goal
-    this._buckOne = buckOne
-    this._buckTwo = buckTwo
     this._starterBuck = starterBuck === 'one'
-    this._goalBucket = ''
-    this._otherBucket = 0
-    this._status_cache = {}
-  }
-
-  movesAux (options: {
-    buckOneCap: number, buckTwoCap: number,
-    buckOneFill: number, buckTwoFill: number,
-    turn: boolean, currentMoves: number, goal: number
-  }): number {
-    let { buckOneCap, buckTwoCap, buckOneFill, buckTwoFill, turn, currentMoves, goal } = options
-    if (buckOneFill === goal || buckTwoFill === goal) {
-      console.log('buckOneFill', buckOneFill, 'buckTwoFill', buckTwoFill)
-      return currentMoves
-    }
-    // if this status was previously visited, ignore it
-    if (this._status_cache[`${buckOneFill} ${buckTwoFill}`]) {
-      return this._status_cache[`${buckOneFill} ${buckTwoFill}`]
-    }
-
-    // Option 1
-    // Pouring one bucket into the other bucket until either: a) the first
-    // bucket is empty b) the second bucket is full
-
-    let [tmpBuckOne, tmpBuckTwo, tmpBuckOneCap, tmpBuckTwoCap] = turn
-      ? [buckOneFill, buckTwoFill, buckOneCap, buckTwoCap]
-      : [buckTwoFill, buckOneFill, buckTwoCap, buckOneCap]
-
-    let maxPour = Math.min(buckTwoCap - tmpBuckTwo, tmpBuckOne)
-
-    let option1Params = {
-      ...options,
-      buckOneFill: turn ? buckOneFill - maxPour : buckOneFill + maxPour,
-      buckTwoFill: turn ? buckTwoFill + maxPour : buckTwoFill - maxPour,
-      currentMoves: currentMoves + 1,
-      turn: !turn
-    }
-
-    let option1moves = this.movesAux(option1Params)
-
-    // Option 2
-    // Emptying a bucket and doing nothing to the other.
-
-    let option2Params = {
-      ...options,
-      buckOneFill: turn ? 0 : buckOneFill,
-      buckTwoFill: turn ? buckTwoFill : 0,
-      turn: !turn,
-      currentMoves: currentMoves + 1
-    }
-
-    let option2moves = this.movesAux(option2Params)
-
-    // Option 3
-    // Filling a bucket and doing nothing to the other.
-
-    let option3Params = {
-      ...options,
-      buckOneFill: turn ? buckOneCap : buckOneFill,
-      buckTwoFill: turn ? buckTwoFill : buckTwoCap,
-      turn: !turn,
-      currentMoves: currentMoves + 1
-    }
-
-    let option3moves = this.movesAux(option3Params)
-
-    console.log('Move #', currentMoves, 'Option 1:', option1moves, 'Option 2:', option2moves, 'Option 3:', option3moves)
-
-    this._status_cache[`${buckOneFill} ${buckTwoFill}`] = Math.min(option1moves, option2moves, option3moves)
-    return this._status_cache[`${buckOneFill} ${buckTwoFill}`]
+    this._buckOne = this._starterBuck ? buckOne : buckTwo
+    this._buckTwo = this._starterBuck ? buckTwo : buckOne
   }
 
   moves (): number {
-    const options = {
-      buckOneCap: this._buckOne,
-      buckTwoCap: this._buckTwo,
-      buckOneFill: this._starterBuck ? this._buckOne : 0,
-      buckTwoFill: this._starterBuck ? 0 : this._buckTwo,
-      turn: this._starterBuck,
-      currentMoves: 1,
-      goal: this._goal
+    if (this._goal % gcd(this._buckOne, this._buckTwo) !== 0) {
+      throw new Error('Not possible to reach the goal')
     }
-    return this.movesAux(options)
+    if (this._goal > this._buckOne && this._goal > this._buckTwo) {
+      throw new Error('Not possible to reach the goal')
+    }
+    if (this._goal === this._buckTwo) {
+      this._goalBucket = 'two'
+      this._otherBucket = this._buckOne
+      return 2
+    }
+    let a = this._buckOne
+    let b = 0
+    this._steps++
+    while (a !== this._goal && b !== this._goal) {
+      const amt = Math.min(a, this._buckTwo - b)
+      a -= amt
+      b += amt
+      this._steps++
+      if (a === this._goal || b === this._goal) {
+        break
+      }
+      if (a === 0) {
+        a = this._buckOne
+        this._steps++
+      }
+      if (b === this._buckTwo) {
+        b = 0
+        this._steps++
+      }
+    }
+    this._goalBucket = a === this._goal
+      ? this._starterBuck ? 'one' : 'two'
+      : this._starterBuck ? 'two' : 'one'
+    this._otherBucket = a === this._goal ? b : a
+    return this._steps
   }
 
   get goalBucket (): string {
@@ -113,10 +69,3 @@ export class TwoBucket {
     return this._otherBucket
   }
 }
-
-const buckOne = 3
-const buckTwo = 5
-const goal = 1
-const starterBuck = 'one'
-const twoBucket = new TwoBucket(buckOne, buckTwo, goal, starterBuck)
-console.log(twoBucket.moves())
